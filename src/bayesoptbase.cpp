30b28ebdd13c657c3d4230abddb64a347e1ea974
rmcantin <rmcantin@unizar.es>
2014-05-14 19:16:25 +0200

    Cleaning demo files

---
+++ src/bayesoptbase.cpp
@@ -0,0 +1,226 @@
+/*
+-------------------------------------------------------------------------
+   This file is part of BayesOpt, an efficient C++ library for 
+   Bayesian optimization.
+
+   Copyright (C) 2011-2014 Ruben Martinez-Cantin <rmcantin@unizar.es>
+ 
+   BayesOpt is free software: you can redistribute it and/or modify it 
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BayesOpt is distributed in the hope that it will be useful, but 
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BayesOpt.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------
+*/
+
+#include "bayesoptbase.hpp"
+
+#include "log.hpp"
+#include "posteriormodel.hpp"
+
+
+namespace bayesopt
+{
+
+
+  BayesOptBase::BayesOptBase(size_t dim, bopt_params parameters):
+    mParameters(parameters), mDims(dim)
+  {
+    // Random seed
+    if (mParameters.random_seed < 0) mParameters.random_seed = std::time(0); 
+    mEngine.seed(mParameters.random_seed);
+
+    // Posterior surrogate model
+    mModel.reset(PosteriorModel::create(dim,parameters,mEngine));
+    
+    // Setting verbose stuff (files, levels, etc.)
+    int verbose = mParameters.verbose_level;
+    if (verbose>=3)
+      {
+	FILE* log_fd = fopen( mParameters.log_filename , "w" );
+	Output2FILE::Stream() = log_fd; 
+	verbose -= 3;
+      }
+
+    switch(verbose)
+      {
+      case 0: FILELog::ReportingLevel() = logWARNING; break;
+      case 1: FILELog::ReportingLevel() = logINFO; break;
+      case 2: FILELog::ReportingLevel() = logDEBUG4; break;
+      default:
+	FILELog::ReportingLevel() = logERROR; break;
+      }
+
+    // Configure iteration parameters
+    if (mParameters.n_init_samples <= 0)
+      {
+	mParameters.n_init_samples = 
+	  static_cast<size_t>(ceil(0.1*mParameters.n_iterations));	
+      }
+  }
+
+  BayesOptBase::~BayesOptBase()
+  { } // Default destructor
+
+
+  void BayesOptBase::stepOptimization()
+  {
+    // Find what is the next point.
+    vectord xNext = nextPoint(); 
+    double yNext = evaluateSampleInternal(xNext);
+
+    // If we are stuck in the same point for several iterations, try a random jump!
+    if (mParameters.force_jump)
+      {
+	if (std::pow(mYPrev - yNext,2) < mParameters.noise)
+	  {
+	    mCounterStuck++;
+	    FILE_LOG(logDEBUG) << "Stuck for "<< mCounterStuck << " steps";
+	  }
+	else
+	  {
+	    mCounterStuck = 0;
+	  }
+	mYPrev = yNext;
+
+	if (mCounterStuck > mParameters.force_jump)
+	  {
+	    FILE_LOG(logINFO) << "Forced random query!";
+	    xNext = samplePoint();
+	    yNext = evaluateSampleInternal(xNext);
+	    mCounterStuck = 0;
+	  }
+      }
+
+    mModel->addSample(xNext,yNext);
+
+    // Update surrogate model
+    bool retrain = ((mParameters.n_iter_relearn > 0) && 
+		    ((mCurrentIter + 1) % mParameters.n_iter_relearn == 0));
+
+    if (retrain)  // Full update
+      {
+	mModel->updateHyperParameters();
+	mModel->fitSurrogateModel();
+      }
+    else          // Incremental update
+      {
+	mModel->updateSurrogateModel();
+      } 
+    plotStepData(mCurrentIter,xNext,yNext);
+    mCurrentIter++;
+  }
+
+  void BayesOptBase::initializeOptimization()
+  {
+    size_t nSamples = mParameters.n_init_samples;
+
+    matrixd xPoints(nSamples,mDims);
+    vectord yPoints(nSamples);
+
+    sampleInitialPoints(xPoints,yPoints);
+    mModel->setSamples(xPoints,yPoints);
+ 
+    if(mParameters.verbose_level > 0)
+      {
+	mModel->plotDataset(logDEBUG);
+      }
+    
+    mModel->updateHyperParameters();
+    mModel->fitSurrogateModel();
+    mCurrentIter = 0;
+
+	mCounterStuck = 0;
+	mYPrev = 0.0;
+  }
+
+
+  void BayesOptBase::optimize(vectord &bestPoint)
+  {
+    initializeOptimization();
+    assert(mDims == bestPoint.size());
+    
+    for (size_t ii = 0; ii < mParameters.n_iterations; ++ii)
+      {      
+	stepOptimization();
+      }
+   
+    bestPoint = getFinalResult();
+  } // optimize
+
+  vectord BayesOptBase::nextPoint()
+  {
+
+    //Epsilon-Greedy exploration (see Bull 2011)
+    if ((mParameters.epsilon > 0.0) && (mParameters.epsilon < 1.0))
+      {
+	randFloat drawSample(mEngine,realUniformDist(0,1));
+	double result = drawSample();
+	FILE_LOG(logINFO) << "Trying random jump with prob:" << result;
+	if (mParameters.epsilon > result)
+	  {
+	    FILE_LOG(logINFO) << "Epsilon-greedy random query!";
+	    return samplePoint();
+	  }
+      }
+
+    //TODO: Try to solve this without bringing the pointer to the
+    //criteria. Right now it does not work with MCMC.
+    vectord Xnext(mDims);    
+
+    // GP-Hedge and related algorithms
+    if (mModel->criteriaRequiresComparison())
+      {
+	bool changed = true;
+
+	mModel->setFirstCriterium();
+	while (changed)
+	  {
+	    findOptimal(Xnext);
+	    changed = mModel->setNextCriterium(Xnext);
+	  }
+	std::string name = mModel->getBestCriteria(Xnext);
+	FILE_LOG(logINFO) << name << " was selected.";
+      }
+    else  // Standard "Bayesian optimization"
+      {
+	FILE_LOG(logDEBUG) << "------ Optimizing criteria ------";
+	findOptimal(Xnext);
+      }
+    return Xnext;
+  }
+
+
+  // Potential inline functions. Moved here to simplify API and header
+  // structure.
+  double BayesOptBase::evaluateCriteria(const vectord& query)
+  {
+    if (checkReachability(query)) return mModel->evaluateCriteria(query);
+    else return 0.0;
+  }
+
+  vectord BayesOptBase::getPointAtMinimum() 
+  { return mModel->getPointAtMinimum(); };
+  
+  double BayesOptBase::getValueAtMinimum()
+  { return mModel->getValueAtMinimum(); };
+
+  ProbabilityDistribution* BayesOptBase::getPrediction(const vectord& query)
+  { return mModel->getPrediction(query); };
+
+   const Dataset* BayesOptBase::getData()
+  { return mModel->getData(); };
+
+  bopt_params* BayesOptBase::getParameters() 
+  {return &mParameters;};
+
+
+} //namespace bayesopt
+
---
+++ src/parameters.cpp
@@ -0,0 +1,203 @@
+/*
+-------------------------------------------------------------------------
+   This file is part of BayesOpt, an efficient C++ library for 
+   Bayesian optimization.
+
+   Copyright (C) 2011-2014 Ruben Martinez-Cantin <rmcantin@unizar.es>
+ 
+   BayesOpt is free software: you can redistribute it and/or modify it 
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BayesOpt is distributed in the hope that it will be useful, but 
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BayesOpt.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------
+*/
+#include <iostream>
+#include "parameters.h"
+
+
+/*-----------------------------------------------------------*/
+/* Default parameters                                        */
+/*-----------------------------------------------------------*/
+
+/* Nonparametric process "parameters" */
+const double KERNEL_THETA    = 1.0;
+const double KERNEL_SIGMA    = 10.0;
+const double MEAN_MU         = 1.0;
+const double MEAN_SIGMA      = 1000.0;
+const double PRIOR_ALPHA     = 1.0;
+const double PRIOR_BETA      = 1.0;
+const double DEFAULT_SIGMA   = 1.0;
+const double DEFAULT_NOISE   = 1e-6;
+
+/* Algorithm parameters */
+const size_t DEFAULT_ITERATIONS         = 190;
+const size_t DEFAULT_INIT_SAMPLES       = 10;
+const size_t DEFAULT_ITERATIONS_RELEARN = 50;
+const size_t DEFAULT_INNER_EVALUATIONS  = 500; /**< Used per dimmension */
+
+const size_t DEFAULT_VERBOSE           = 1;
+
+
+
+learning_type str2learn(const char* name)
+{
+  if      (!strcmp(name,"L_FIXED")     || !strcmp(name,"fixed"))     return L_FIXED;
+  else if (!strcmp(name,"L_EMPIRICAL") || !strcmp(name,"empirical")) return L_EMPIRICAL;
+  else if (!strcmp(name,"L_DISCRETE")  || !strcmp(name,"discrete"))  return L_DISCRETE;
+  else if (!strcmp(name,"L_MCMC")      || !strcmp(name,"mcmc"))      return L_MCMC;
+  else return L_ERROR;
+}
+
+const char* learn2str(learning_type name)
+{
+  switch(name)
+    {
+    case L_FIXED:     return "L_FIXED"; 
+    case L_EMPIRICAL: return "L_EMPIRICAL"; 
+    case L_DISCRETE:  return "L_DISCRETE"; 
+    case L_MCMC:      return "L_MCMC"; 
+    case L_ERROR:
+    default: return "ERROR!";
+    }
+}
+
+score_type str2score(const char* name)
+{
+  if      (!strcmp(name,"SC_MTL")   || !strcmp(name,"mtl"))   return SC_MTL;
+  else if (!strcmp(name,"SC_ML")    || !strcmp(name,"ml"))    return SC_ML;
+  else if (!strcmp(name,"SC_MAP")   || !strcmp(name,"map"))   return SC_MAP;
+  else if (!strcmp(name,"SC_LOOCV") || !strcmp(name,"loocv")) return SC_LOOCV;
+  else return SC_ERROR;
+}
+
+const char* score2str(score_type name)
+{
+  switch(name)
+    {
+    case SC_MTL:   return "SC_MTL"; 
+    case SC_ML:    return "SC_ML"; 
+    case SC_MAP:   return "SC_MAP"; 
+    case SC_LOOCV: return "SC_LOOCV"; 
+    case SC_ERROR:
+    default: return "ERROR!";
+    }
+}
+
+
+void set_kernel(bopt_params* params, const char* name)
+{
+  strcpy(params->kernel.name, name);
+};
+
+void set_mean(bopt_params* params, const char* name)
+{
+  strcpy(params->mean.name, name);
+};
+
+void set_criteria(bopt_params* params, const char* name)
+{
+  strcpy(params->crit_name, name);
+};
+
+void set_surrogate(bopt_params* params, const char* name)
+{
+  strcpy(params->surr_name, name);
+};
+
+void set_log_file(bopt_params* params, const char* name)
+{
+  strcpy(params->log_filename, name);
+};
+
+void set_load_file(bopt_params* params, const char* name)
+{
+  strcpy(params->load_filename, name);
+};
+
+void set_save_file(bopt_params* params, const char* name)
+{
+  strcpy(params->save_filename, name);
+};
+
+void set_learning(bopt_params* params, const char* name)
+{
+  params->l_type = str2learn(name);
+};
+
+void set_score(bopt_params* params, const char* name)
+{
+  params->sc_type = str2score(name);
+};
+
+bopt_params initialize_parameters_to_default(void)
+{
+  kernel_parameters kernel;
+  kernel.name = new char[128];
+  strcpy(kernel.name,"kMaternARD5");
+
+  kernel.hp_mean[0] = KERNEL_THETA;
+  kernel.hp_std[0]  = KERNEL_SIGMA;
+  kernel.n_hp       = 1;
+  
+  mean_parameters mean;
+  mean.name = new char[128];
+  strcpy(mean.name,"mConst");
+
+  mean.coef_mean[0] = MEAN_MU;
+  mean.coef_std[0]  = MEAN_SIGMA;
+  mean.n_coef       = 1;
+  
+
+  bopt_params params;
+
+  params.n_iterations       = DEFAULT_ITERATIONS;
+  params.n_inner_iterations = DEFAULT_INNER_EVALUATIONS;
+  params.n_init_samples     = DEFAULT_INIT_SAMPLES;
+  params.n_iter_relearn     = DEFAULT_ITERATIONS_RELEARN;
+
+  params.init_method      =  1;
+  params.random_seed      = -1;
+
+  params.verbose_level = DEFAULT_VERBOSE;
+  params.log_filename  = new char[128];
+  strcpy(params.log_filename,"bayesopt.log");
+
+  params.load_save_flag = 0;
+  params.load_filename = new char[128];
+  strcpy(params.log_filename,"bayesopt.dat");
+  params.save_filename = new char[128];
+  strcpy(params.log_filename,"bayesopt.dat");
+
+  params.surr_name = new char[128];
+  //  strcpy(params.surr_name,"sStudentTProcessNIG");
+  strcpy(params.surr_name,"sGaussianProcess");
+
+  params.sigma_s = DEFAULT_SIGMA;
+  params.noise   = DEFAULT_NOISE;
+  params.alpha   = PRIOR_ALPHA;
+  params.beta    = PRIOR_BETA;
+
+  params.l_all   = 0;
+  params.l_type  = L_EMPIRICAL;
+  params.sc_type = SC_MAP;
+
+  params.epsilon = 0.0;
+  params.force_jump = 20;
+  
+  params.crit_name = new char[128];
+  strcpy(params.crit_name,"cEI");
+  params.n_crit_params = 0;
+
+  params.kernel = kernel;
+  params.mean = mean;
+
+  return params;
+}
