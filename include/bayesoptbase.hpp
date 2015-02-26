d724988413ffc19aae0d4602b3f26cee48f97ac1
jgbarcos <jgbarcos@localhost>
2015-02-26 23:48:15 +0100

    Added dummy functions to save and restore the state of an optimization process

---
+++ include/bayesoptbase.hpp
@@ -0,0 +1,216 @@
+
+/**  \file bayesoptbase.hpp \brief BayesOpt common module for interfaces */
+/*
+-------------------------------------------------------------------------
+   This file is part of BayesOpt, an efficient C++ library for 
+   Bayesian optimization.
+
+   Copyright (C) 2011-2014 Ruben Martinez-Cantin <rmcantin@unizar.es>
+ 
+   BayesOpt is free software: you can redistribute it and/or modify it 
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BayesOpt is distributed in the hope that it will be useful, but 
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BayesOpt.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------
+*/
+
+
+#ifndef  _BAYESOPTBASE_HPP_
+#define  _BAYESOPTBASE_HPP_
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/random.hpp>
+#include "parameters.h"
+#include "specialtypes.hpp"
+//#include "posteriormodel.hpp"
+
+
+/**
+ * Namespace of the library interface
+ */
+namespace bayesopt {
+
+
+  //Forward declaration
+  class PosteriorModel;
+  class ProbabilityDistribution;
+  class Dataset;
+
+  /** \addtogroup BayesOpt
+   *  \brief Main module for Bayesian optimization
+   */
+  /*@{*/
+
+  /**
+   * \brief Abstract module for Bayesian optimization.
+   *
+   * This module provides Bayesian optimization using different
+   * non-parametric processes (Gaussian process or Student's t
+   * process) as distributions over surrogate functions.
+   *
+   * \see ContinuousModel for implementations of this module for
+   * a continuous input spaces
+   *
+   * \see DiscreteModel for implementations of this module for
+   * a discrete input spaces or categorical input variables
+   */
+  class BAYESOPT_API BayesOptBase
+  {
+  public:
+    /** 
+     * Constructor
+     * @param params set of parameters (see parameters.h)
+     */
+    BayesOptBase(size_t dim, bopt_params params);
+
+    /** 
+     * Default destructor
+     */
+    virtual ~BayesOptBase();
+
+    /** 
+     * \brief Function that defines the actual function to be optimized.
+     * This function must be modified (overriden) according to the
+     * specific problem.
+     *
+     * @param query point to be evaluated. 
+     * @return value of the function at the point evaluated.
+     */
+    virtual double evaluateSample( const vectord &query ) = 0;
+    
+
+    /** 
+     * \brief This function checks if the query is valid or not. It can
+     * be used to introduce arbitrary constrains. Since the Gaussian
+     * process assumes smoothness, constrains are managed by the inner
+     * optimizer (e.g.:DIRECT), being highly time consuming. If the
+     * constrain is very tricky, DIRECT will need much more function
+     * evaluations.
+     *
+     * Note: This function is experimental. Thus it is not made pure virtual.
+     * Using it is completely optional.
+     * 
+     * @param query point to be evaluated.
+     * 
+     * @return boolean value showing if the the function is valid at
+     *         the query point or not.
+     */ 
+    virtual bool checkReachability( const vectord &query )
+    { return true; };
+
+
+    /** 
+     * \brief Execute the optimization process of the function defined
+     * in evaluateSample.
+     * 
+     * @see evaluateSample
+     * @see checkReachability
+     *
+     * @param bestPoint returns point with the optimum value in a ublas::vector.
+     */
+    void optimize(vectord &bestPoint);
+
+    /** 
+     * \brief Execute ONE step the optimization process of the
+     * function defined in evaluateSample.  
+     */  
+    void stepOptimization();
+
+    /** Initialize the optimization process.  */
+    void initializeOptimization();
+    
+    // TODO (Javier): saveOptimization is a dummy function subject to change, being developed for Restore functionality.
+    /** Saves the current state of the optimization process into a struct. */
+    void saveOptimization( /* bopt_state &state (struct not defined yet) */ );
+    
+    // TODO (Javier): restoreOptimization is a dummy function subject to change, being developed for Restore functionality.
+    /** Restores the optimization process of a previous execution */
+    void restoreOptimization(/* bopt_state state (struct not defined yet) */);
+    
+    /** Once the optimization has been perfomed, return the optimal point. */
+    virtual vectord getFinalResult() = 0;
+
+    ProbabilityDistribution* getPrediction(const vectord& query);
+    const Dataset* getData();
+    bopt_params* getParameters();
+    double getValueAtMinimum();
+    double evaluateCriteria(const vectord& query);
+
+  protected:
+    vectord getPointAtMinimum();
+
+    /** 
+     * Print data for every step according to the verbose level
+     * 
+     * @param iteration 
+     * @param xNext 
+     * @param yNext 
+     */
+    virtual void plotStepData(size_t iteration, const vectord& xNext,
+			      double yNext) = 0;
+
+
+    /** 
+     * \brief Wrapper for the target function normalize in the hypercube
+     * [0,1]
+     * @param query point to evaluate in [0,1] hypercube
+     * @return actual return value of the target function
+     */
+    virtual double evaluateSampleInternal( const vectord &query ) = 0;
+
+
+    /** 
+     * \brief Returns the optimal point acording to certain criteria
+     * @see evaluateCriteria
+     *
+     * @param xOpt optimal point
+     */
+    virtual void findOptimal(vectord &xOpt) = 0;
+  
+    /** Selects the initial set of points to build the surrogate model. */
+    virtual void sampleInitialPoints(matrixd& xPoints, vectord& yPoints) = 0;
+
+    /** Sample a single point in the input space. Used for epsilon
+	greedy exploration. */
+    virtual vectord samplePoint() = 0;
+
+  protected:
+    bopt_params mParameters;                    ///< Configuration parameters
+    size_t mDims;                                   ///< Number of dimensions
+    size_t mCurrentIter;                        ///< Current iteration number
+    boost::mt19937 mEngine;                      ///< Random number generator
+
+  private:
+    boost::scoped_ptr<PosteriorModel> mModel;
+    double mYPrev;
+    size_t mCounterStuck;
+  private:
+
+    BayesOptBase();
+
+    /** 
+     * \brief Selects the next point to evaluate according to a certain
+     * criteria or metacriteria
+     * 
+     * @return next point to evaluate
+     */
+    vectord nextPoint();  
+
+  };
+
+  /**@}*/
+
+
+
+} //namespace bayesopt
+
+
+#endif
---
+++ src/bayesoptbase.cpp
@@ -0,0 +1,290 @@
+/*
+-------------------------------------------------------------------------
+   This file is part of BayesOpt, an efficient C++ library for 
+   Bayesian optimization.
+
+   Copyright (C) 2011-2014 Ruben Martinez-Cantin <rmcantin@unizar.es>
+ 
+   BayesOpt is free software: you can redistribute it and/or modify it 
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   BayesOpt is distributed in the hope that it will be useful, but 
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with BayesOpt.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------
+*/
+
+#include <ctime>
+#include "bayesoptbase.hpp"
+
+#include "log.hpp"
+#include "posteriormodel.hpp"
+
+
+namespace bayesopt
+{
+  BayesOptBase::BayesOptBase(size_t dim, bopt_params parameters):
+    mParameters(parameters), mDims(dim)
+  {
+    // Random seed
+    if (mParameters.random_seed < 0) mParameters.random_seed = std::time(0); 
+    mEngine.seed(mParameters.random_seed);
+
+    // Posterior surrogate model
+    mModel.reset(PosteriorModel::create(dim,parameters,mEngine));
+    
+    // Setting verbose stuff (files, levels, etc.)
+    int verbose = mParameters.verbose_level;
+    if (verbose>=3)
+      {
+	FILE* log_fd = fopen( mParameters.log_filename , "w" );
+	Output2FILE::Stream() = log_fd; 
+	verbose -= 3;
+      }
+
+    switch(verbose)
+      {
+      case 0: FILELog::ReportingLevel() = logWARNING; break;
+      case 1: FILELog::ReportingLevel() = logINFO; break;
+      case 2: FILELog::ReportingLevel() = logDEBUG4; break;
+      default:
+	FILELog::ReportingLevel() = logERROR; break;
+      }
+
+    // Configure iteration parameters
+    if (mParameters.n_init_samples <= 0)
+      {
+	mParameters.n_init_samples = 
+	  static_cast<size_t>(ceil(0.1*mParameters.n_iterations));	
+      }
+  }
+
+  BayesOptBase::~BayesOptBase()
+  { } // Default destructor
+
+
+  void BayesOptBase::stepOptimization()
+  {
+    // Find what is the next point.
+    vectord xNext = nextPoint(); 
+    double yNext = evaluateSampleInternal(xNext);
+
+    // If we are stuck in the same point for several iterations, try a random jump!
+    if (mParameters.force_jump)
+      {
+        if (std::pow(mYPrev - yNext,2) < mParameters.noise)
+          {
+            mCounterStuck++;
+            FILE_LOG(logDEBUG) << "Stuck for "<< mCounterStuck << " steps";
+          }
+        else
+          {
+            mCounterStuck = 0;
+          }
+        mYPrev = yNext;
+
+        if (mCounterStuck > mParameters.force_jump)
+          {
+            FILE_LOG(logINFO) << "Forced random query!";
+            xNext = samplePoint();
+            yNext = evaluateSampleInternal(xNext);
+            mCounterStuck = 0;
+          }
+      }
+
+    mModel->addSample(xNext,yNext);
+
+    // Update surrogate model
+    bool retrain = ((mParameters.n_iter_relearn > 0) && 
+		    ((mCurrentIter + 1) % mParameters.n_iter_relearn == 0));
+
+    if (retrain)  // Full update
+      {
+        mModel->updateHyperParameters();
+        mModel->fitSurrogateModel();
+      }
+    else          // Incremental update
+      {
+        mModel->updateSurrogateModel();
+      } 
+    plotStepData(mCurrentIter,xNext,yNext);
+    mModel->updateCriteria(xNext);
+    mCurrentIter++;
+    
+    // Save state
+    if(mParameters.load_save_flag == 2 || mParameters.load_save_flag == 3){
+        //bopt_state state;
+        saveOptimization(/* state */);
+        //saveState(mParameters.save_filename, state); // function that will save the state
+    }
+  }
+  
+  // TODO (Javier): saveOptimization is a dummy function subject to change, being developed for Restore functionality.
+  void BayesOptBase::saveOptimization( /* bopt_state &state (struct not defined yet) */ ){
+    /*
+     * Details to be considered:
+     *  - Have access to all necessary data: 
+     *      BayesOptBase members, PosteriorModel samples...
+     *  - Define behaviour when save fails (probably print/log a warning but continue) 
+     *
+     * (... still gathering info about data needed to be saved into state.)
+     */
+     
+     //state.mCurrentIter = mCurrentIter;
+     // ... etc
+  }
+
+  // TODO (Javier): restoreOptimization is a dummy function subject to change, being developed for Restore functionality.
+  void BayesOptBase::restoreOptimization(/* bopt_state state (struct not defined yet) */){
+    /*
+     * Details to consider:
+     *  - If a random seed was auto-generated in the previous opt, should it be required to mantain the same seed?
+     *      if yes, then state should have the seed value and be applied instead of the bopt_params mParameters seed value.
+     *  - Should this be in constructor? because it reinitializes things initialized in the public constructor
+     *      but it also acts as initializeOptimization(), so currently decided to leave it in outside constructor.
+     */
+     
+    // Posterior surrogate model
+    //mModel.reset(PosteriorModel::create(dim, state.parameters, mEngine));
+    
+    // Put state samples into model
+    //mModel->setSamples(state.xSamples, state.ySamples);
+    
+    if(mParameters.verbose_level > 0)
+      {
+        mModel->plotDataset(logDEBUG);
+      }
+      
+    //TODO (Javier): this could be not so easy, (... still gathering info about data needed to be placed.) 
+    //mParameters = state.parameters;  
+    
+    //TODO (Javier): check if only with the samples, the posterior model can be recreated/recalculated.
+    mModel->updateHyperParameters();
+    mModel->fitSurrogateModel();
+    
+    //mCurrentIter = state.mCurrentIter;
+
+	//mCounterStuck = state.mCounterStuck;
+	//mYPrev = state.mYPrev;
+  }
+
+  void BayesOptBase::initializeOptimization()
+  {
+    size_t nSamples = mParameters.n_init_samples;
+
+    matrixd xPoints(nSamples,mDims);
+    vectord yPoints(nSamples);
+
+    sampleInitialPoints(xPoints,yPoints);
+    mModel->setSamples(xPoints,yPoints);
+ 
+    if(mParameters.verbose_level > 0)
+      {
+	mModel->plotDataset(logDEBUG);
+      }
+    
+    mModel->updateHyperParameters();
+    mModel->fitSurrogateModel();
+    mCurrentIter = 0;
+
+	mCounterStuck = 0;
+	mYPrev = 0.0;
+  }
+
+
+  void BayesOptBase::optimize(vectord &bestPoint)
+  {
+    // Restore state from file
+    if(mParameters.load_save_flag == 1 || mParameters.load_save_flag == 3){
+        //bopt_state state;
+        //loadState(mParameters.load_filename, state); // function that will load and parse the state
+        restoreOptimization(/* state */);
+    }
+    // Initialize a new state
+    else{
+        initializeOptimization();
+    }
+    
+    assert(mDims == bestPoint.size());
+    
+    for (size_t ii = mCurrentIter; ii < mParameters.n_iterations; ++ii)
+      {      
+        stepOptimization();
+      }
+   
+    bestPoint = getFinalResult();
+  } // optimize
+
+  vectord BayesOptBase::nextPoint()
+  {
+
+    //Epsilon-Greedy exploration (see Bull 2011)
+    if ((mParameters.epsilon > 0.0) && (mParameters.epsilon < 1.0))
+      {
+	randFloat drawSample(mEngine,realUniformDist(0,1));
+	double result = drawSample();
+	FILE_LOG(logINFO) << "Trying random jump with prob:" << result;
+	if (mParameters.epsilon > result)
+	  {
+	    FILE_LOG(logINFO) << "Epsilon-greedy random query!";
+	    return samplePoint();
+	  }
+      }
+
+    vectord Xnext(mDims);    
+
+    // GP-Hedge and related algorithms
+    if (mModel->criteriaRequiresComparison())
+      {
+	bool changed = true;
+
+	mModel->setFirstCriterium();
+	while (changed)
+	  {
+	    findOptimal(Xnext);
+	    changed = mModel->setNextCriterium(Xnext);
+	  }
+	std::string name = mModel->getBestCriteria(Xnext);
+	FILE_LOG(logINFO) << name << " was selected.";
+      }
+    else  // Standard "Bayesian optimization"
+      {
+	FILE_LOG(logDEBUG) << "------ Optimizing criteria ------";
+	findOptimal(Xnext);
+      }
+    return Xnext;
+  }
+
+
+  // Potential inline functions. Moved here to simplify API and header
+  // structure.
+  double BayesOptBase::evaluateCriteria(const vectord& query)
+  {
+    if (checkReachability(query)) return mModel->evaluateCriteria(query);
+    else return 0.0;
+  }
+
+  vectord BayesOptBase::getPointAtMinimum() 
+  { return mModel->getPointAtMinimum(); };
+  
+  double BayesOptBase::getValueAtMinimum()
+  { return mModel->getValueAtMinimum(); };
+
+  ProbabilityDistribution* BayesOptBase::getPrediction(const vectord& query)
+  { return mModel->getPrediction(query); };
+
+   const Dataset* BayesOptBase::getData()
+  { return mModel->getData(); };
+
+  bopt_params* BayesOptBase::getParameters() 
+  {return &mParameters;};
+
+
+} //namespace bayesopt
+
